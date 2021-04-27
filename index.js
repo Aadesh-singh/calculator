cc2ce7b0f2fa9825e6e08d99974dfeab69748002
Aadesh-singh <aadeshsingh146@gmail.com>
2021-04-27 13:22:24 +0530

    added styles and inversion and square root to the file.

--- index.js
+++ index.js
@@ -1,4 +1,42 @@
-var count = 0;
 
-document.getElementById('screen').innerHTML= count;
+    document.getElementById('display').innerHTML = "0";
+function forclear(){
+    document.getElementById('display').innerHTML = "0";
+    
+}
+function removeZero(){
+    var value = document.getElementById('display').innerHTML;
+    if( value == "0"){
+        value = " ";
+        document.getElementById('display').innerHTML = value;
+    }
+}
 
+function mod(){
+    var value = document.getElementById('display').innerHTML;
+    value /= 100;
+    document.getElementById('display').innerHTML = value;
+}
+
+function forDisplay(value){
+    removeZero();
+    document.getElementById('display').innerHTML += value;
+}
+
+function solve(){
+    removeZero();
+    var equation = document.getElementById('display').innerHTML;
+    document.getElementById('display').innerHTML = eval(equation);
+}
+
+function sq (){
+    removeZero();
+    var value = document.getElementById('display').innerHTML;
+    var res = Math.sqrt(value);
+    document.getElementById('display').innerHTML = res;
+}
+function invert() {
+    var val = document.getElementById('display').innerHTML;
+    val *= -1;
+    document.getElementById('display').innerHTML = val;
+}
